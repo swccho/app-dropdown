e183f6e6811c7d97bcb42c9231cdb8756bac791a
Noyon Ahammed Sojon <ahammedsojon000@gmail.com>
2024-05-23 18:28:59 +0600

    multiple drop down created

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,127 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <div class="dropdown">
-      <button class="btn btn-secondary toggle-btn" @click="toggleDropDown">
-        Dropdown button
-      </button>
-      <DropDown :dropItems="dropArr" ref="dropDown" />
+    <div class="grid">
+      <div class="card">
+        <DropDown :dropItems="dropArr" :buttonText="`Dropdown Toggle`"/>
+        <div class="html-content">
+          <pre>{{ htmlContent[0] }}</pre>
+        </div>
+        <div class="desc">
+          Here we can pass prop. We can see above code there has two prop.
+          <mark>dropItems</mark>
+          is an array that contains drop
+          down items. And
+          <mark>buttonText</mark>
+          contains button inner text.
+        </div>
+      </div>
+
+      <div class="card">
+        <DropDown :dropItems="dropArr" :theme="'seabrook'" :buttonText="`Dropdown Toggle`"/>
+        <div class="html-content">
+          <pre>{{ htmlContent[1] }}</pre>
+        </div>
+        <div class="desc">
+          Here we can see
+          <mark>theme</mark>
+          prop that contains color of drop down button and drop down menu. Default value is
+          <mark>#273c75</mark>.
+        </div>
+      </div>
+
+      <div class="card">
+        <DropDown :dropItems="dropArr" :theme="'periwinkle'" :buttonText="`Dropdown Toggle`"
+                  :icon="'/images/down.png'"/>
+        <div class="html-content">
+          <pre>{{ htmlContent[2] }}</pre>
+        </div>
+        <div class="desc">
+          Here we can see
+          <mark>icon</mark>
+          prop that contains an image path of drop down button's icon.
+        </div>
+      </div>
+
+      <div class="card">
+        <DropDown :dropItems="dropArr" :theme="'nasturcian'" :position="'right'" :buttonText="`Dropdown Toggle`"
+                  :icon="'/images/down.png'"/>
+        <div class="html-content">
+          <pre>{{ htmlContent[3] }}</pre>
+        </div>
+        <div class="desc">
+          Here we can see
+          <mark>position</mark>
+          prop that contains position of dropdown-menu whether it's start from left or right. Default value is
+          <mark>position</mark>.
+        </div>
+      </div>
+
+      <div class="card">
+        <DropDown :dropItems="dropArr" :theme="'aqua'" :position="'left'" :buttonText="`Dropdown Toggle`"
+                  :icon="'/images/down.png'"/>
+        <div class="html-content">
+          <pre>{{ htmlContent[4] }}</pre>
+        </div>
+        <div class="desc">
+          Here we can see
+          <mark>position</mark>
+          prop that contains position of dropdown-menu whether it's start from left or right. Default value is
+          <mark>left</mark>. <mark>theme</mark>
+          prop that contains color of drop down button and drop down menu. <mark>buttonText</mark> prop that
+          contains button inner text. <mark>icon</mark>
+          prop that contains an image path of drop down button's icon.
+        </div>
+      </div>
+
+      <div class="card">
+        <DropDown :dropItems="dropArr" :theme="'blueberry'" :position="'right'" :buttonText="`Dropdown Toggle`"
+                  :icon="'/images/down.png'"/>
+        <div class="html-content">
+          <pre>{{ htmlContent[5] }}</pre>
+        </div>
+        <div class="desc">
+          Here we can see
+          <mark>position</mark>
+          prop that contains position of dropdown-menu whether it's start from left or right. Default value is
+          <mark>left</mark>. <mark>theme</mark>
+          prop that contains color of drop down button and drop down menu. <mark>buttonText</mark> prop that
+          contains button inner text. <mark>icon</mark>
+          prop that contains an image path of drop down button's icon.
+        </div>
+      </div>
+
     </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
 import DropDown from "@/components/DropDown";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
     DropDown
   },
-  data () {
+  data() {
     return {
       dropArr: [
-          'About Us',
-          'Service',
-          'Pricing',
-          'Contact Us',
+        'About Us',
+        'Service',
+        'Pricing',
+        'Contact Us',
       ],
-      isDropDownActive: false
+      htmlContent: [
+        '<DropDown :dropItems="dropArr" :buttonText="`Dropdown Toggle`" />',
+        '<DropDown :dropItems="dropArr" :theme="\'seabrook\'" :buttonText="`Dropdown Toggle` :icon="\'/images/down.png\'"/>',
+        '<DropDown :dropItems="dropArr" :theme="\'periwinkle\'" :buttonText="`Dropdown Toggle` :icon="\'/images/down.png\'"/>',
+        '<DropDown :dropItems="dropArr" :theme="\'nasturcian\'" :position="\'right\'" :buttonText="`Dropdown Toggle` :icon="\'/images/down.png\'"/>',
+        '<DropDown :dropItems="dropArr" :theme="\'aqua\'" :position="\'right\'" :buttonText="`Dropdown Toggle` :icon="\'/images/down.png\'"/>',
+        '<DropDown :dropItems="dropArr" :theme="\'blueberry\'" :position="\'right\'" :buttonText="`Dropdown Toggle` :icon="\'/images/down.png\'"/>',
+      ]
     }
   },
-  methods: {
-    toggleDropDown(){
-      this.$refs.dropDown.handleDropDown()
-    }
+  methods: {},
+  mounted() {
   }
 }
 </script>
--- src/components/DropDown.vue
+++ src/components/DropDown.vue
@@ -1,33 +1,57 @@
 <template>
-  <ul :class="{'dropdown-menu': true, 'active': isDropDownActive}">
-    <li v-for="(each, index) in dropItems" :key="index">
-      <a href="">{{each}}</a>
-    </li>
-  </ul>
+ <div class="dropdown" :class="[theme, position, transform]">
+   <button class="btn toggle-btn" @click="handleDropDown($event)" ref="toggleBtn">
+     {{buttonText}} <img :src="icon" alt="icon" v-if="icon">
+   </button>
+   <ul class="dropdown-menu">
+     <li v-for="(each, index) in dropItems" :key="index">
+       <a href="javascript:void(0)">{{each}}</a>
+     </li>
+   </ul>
+ </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: ['msg', 'dropItems'],
+  name: 'DropDown',
+  props: ['dropItems', 'theme', 'buttonText', 'icon', 'position', 'transform'],
   data(){
     return {
       isDropDownActive: false
     }
   },
   methods: {
-    handleDropDown(){
-      this.isDropDownActive = !this.isDropDownActive
+    handleDropDown(e){
+      const dropDowns = document.querySelectorAll('.dropdown .dropdown-menu');
+      const dropDown = e.target.closest('.dropdown').querySelector('.dropdown-menu');
+      // const toggleBtn = e.target.closest('.dropdown').querySelector('.toggleBtn');
+      dropDowns.forEach((elem) => {
+        if(elem !== dropDown){
+          elem.classList.remove('active')
+        }else{
+          if(elem.classList.contains('active')){
+            elem.classList.remove('active')
+                e.target.classList.remove('active')
+          }else{
+            elem.classList.add('active')
+            e.target.classList.add('active')
+          }
+        }
+      })
     }
   },
 
   mounted() {
+    console.log(this.theme)
     window.addEventListener('click', (e) => {
-      const toggleButton = document.querySelector('.dropdown .toggle-btn');
-      const dropDown = document.querySelector('.dropdown-menu');
-      if(!dropDown.contains(e.target) && !toggleButton.contains(e.target)){
-        this.isDropDownActive = false;
-      }
+      const dropDowns = document.querySelectorAll('.dropdown-menu');
+      dropDowns.forEach((elem) => {
+        if(!elem.contains(e.target) && !elem.closest('.dropdown').querySelector('.toggle-btn').contains(e.target)){
+          elem.classList.remove('active')
+          elem.closest('.dropdown').querySelector('.toggle-btn').classList.remove('active');
+        }
+      })
+
     })
   }
 }
