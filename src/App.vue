32f9d2177377cd0259ea32be4c913c7fbcfec58c
Noyon Ahammed Sojon <ahammedsojon000@gmail.com>
2024-05-23 11:38:10 +0600

    dorp down menu created globally

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,40 @@
 <template>
   <div id="app">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <div class=""></div>
+    <div class="dropdown">
+      <button class="btn btn-secondary toggle-btn" @click="toggleDropDown">
+        Dropdown button
+      </button>
+      <DropDown :dropItems="dropArr" ref="dropDown" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import DropDown from "@/components/DropDown";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    DropDown
+  },
+  data () {
+    return {
+      dropArr: [
+          'About Us',
+          'Service',
+          'Pricing',
+          'Contact Us',
+      ],
+      isDropDownActive: false
+    }
+  },
+  methods: {
+    toggleDropDown(){
+      this.$refs.dropDown.handleDropDown()
+    }
   }
 }
 </script>
--- src/components/DropDown.vue
+++ src/components/DropDown.vue
@@ -1,13 +1,35 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-  </div>
+  <ul :class="{'dropdown-menu': true, 'active': isDropDownActive}">
+    <li v-for="(each, index) in dropItems" :key="index">
+      <a href="">{{each}}</a>
+    </li>
+  </ul>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: ['msg']
+  props: ['msg', 'dropItems'],
+  data(){
+    return {
+      isDropDownActive: false
+    }
+  },
+  methods: {
+    handleDropDown(){
+      this.isDropDownActive = !this.isDropDownActive
+    }
+  },
+
+  mounted() {
+    window.addEventListener('click', (e) => {
+      const toggleButton = document.querySelector('.dropdown .toggle-btn');
+      const dropDown = document.querySelector('.dropdown-menu');
+      if(!dropDown.contains(e.target) && !toggleButton.contains(e.target)){
+        this.isDropDownActive = false;
+      }
+    })
+  }
 }
 </script>
 
